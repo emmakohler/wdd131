f6b9f490df3b6f2d998c019f1f89b5bc4ff5ef04
emmakohler <156350110+emmakohler@users.noreply.github.com>
2025-10-27 11:40:11 -0600

    fixing blog by taking out extra js

--- prove/blog/blog.js
+++ prove/blog/blog.js
@@ -62,19 +62,3 @@ const articles = [
     
     });
 
-
-
-
-
-
-let html = `<article class="book">
-                <div class="book-details">
-                    <p><em>July 5, 2022</em></p>
-                    <p>10 - 14</p>
-                    <p>Fantasy</p>
-                    <p><strong>Ratings:</strong></p> <span aria-label="4 out of 5 stars" role="img">⭐⭐⭐⭐</span></p>
-                </div>
-                <h2>Septimus Heap Book One: Magyk</h2>
-                <img src="Magkycover2.jpg" alt="">
-                <p id="words">If you enjoy short stories about seventh sons of seventh sons and magyk, this book is for you.</p>
-            </article>`
