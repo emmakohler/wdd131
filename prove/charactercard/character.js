54a8ef4a31ccada019e6645ada28f461777be865
emmakohler <156350110+emmakohler@users.noreply.github.com>
2025-11-08 23:06:46 -0700

    character card

---
+++ prove/charactercard/character.js
@@ -0,0 +1,25 @@
+const cCard = {
+    class: 'Desert Beast',
+    level:'1',
+    health: '100',
+    character: 'Thorny Devil',
+    image: 'images/baby-thornydevil.png'
+};
+
+function cardTemplate(cCard) {
+    return `<img class='image'src="${image}">
+        <h2 class="name">${character}</h2>
+        <div class="stats">
+            <p><strong>Class:</strong>${class}</p>
+            <p><strong>Level:</strong>${level}</p>
+            <p><strong>Health:</strong>${health}</p>
+        </div>
+        <div class="buttons">
+            <button id="attacked">Attacked</button>
+            <button id="levelup">Level Up</button>
+        </div>`
+};
+
+
+// make a level up function
+// make an attack function
